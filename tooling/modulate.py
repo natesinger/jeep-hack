9d6ba18197d2a600081dc65da7b66628adf9a5a8
Nathaniel Singer <nathaniel@singer.cloud>
2021-06-11 03:39:57 -0700

    instantiation

---
+++ tooling/modulate.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Key Fob Listen
+# GNU Radio version: 3.8.2.0
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import gr
+from gnuradio.filter import firdes
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import qtgui
+import sys
+import signal
+import time
+
+class listen(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Key Fob Listen")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Key Fob Listen")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "listen")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+        
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 20e6
+        with open('bitstream.bin','r') as io: self.bitstream = bitstream = [int(c) for c in io.read() if (c == '0') or (c == '1')]
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.blocks_vector_source_x_2 = blocks.vector_source_c(bitstream, False, 1, [])
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_gr_complex*1, 4167)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'generated_wave.bin', False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_SIN_WAVE, 439e6, 1, 0, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_vector_source_x_2, 0), (self.blocks_repeat_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "listen")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+
+    def get_bitstream(self):
+        return self.bitstream
+
+    def set_bitstream(self, bitstream):
+        self.bitstream = bitstream
+        self.blocks_vector_source_x_2.set_data(self.bitstream, [])
+
+
+
+
+
+def main(top_block_cls=listen, options=None):
+
+    print('1')
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    print('2')
+
+    tb = top_block_cls()
+
+    print('3')
+
+    tb.start()
+
+    print('4')
+
+    tb.show()
+
+    print('5')
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    print('6')
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+
+    print('7')
+
+    qapp.aboutToQuit.connect(quitting)
+    
+    print('8')
+    
+    #qapp.exec_()
+    
+    print('9')
+
+if __name__ == '__main__':
+    main()
